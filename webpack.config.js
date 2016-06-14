4c8d0caa9a5e062ed44ac421da769f4530f5c4e1
Oscar Dominguez <oscardc@gmx.com>
2016-06-14 16:40:37 +0200

    chore: Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// In webpack.config.js
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
+    ]
+  },
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
