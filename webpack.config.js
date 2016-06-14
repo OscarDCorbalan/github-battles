37dab88a39fa935e8ba0dc42a8c0071b594e1e46
Oscar Dominguez <oscardc@gmx.com>
2016-06-14 16:41:27 +0200

    chore: Set up babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,14 @@ module.exports = {
   entry: [
     './app/index.js'
   ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
   module: {
     loaders: [
-      {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
-  },
-  output: {
-    filename: "index_bundle.js",
-    path: __dirname + '/dist'
   },
   plugins: [HTMLWebpackPluginConfig]
 };
